dd26a9ed579e8acd52795dcce5967ae9a45a9c7f
MicheleRoar <micheleleone@outlook.com>
2024-04-22 12:29:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/MicheleRoar/TaxonMatch',
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
     install_requires=required,  
 )
